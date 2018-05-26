497ee55c0f6552070a82f38d26a8b493e64d4ef9
worakitw <worakitw@gmail.com>
2018-05-26 22:13:45 +0700

    upload2

--- pages/upload.vue
+++ pages/upload.vue
@@ -4,18 +4,34 @@
       <v-flex xs12 sm6 offset-md3>
         <v-card>
           <v-toolbar color="indigo" dark>
-            <v-toolbar-title>ลงทะเบียน</v-toolbar-title>
+            <v-toolbar-title>Upload File Grade</v-toolbar-title>
           </v-toolbar>
+          
           <v-card-text>
             <span class="title"></span>
-            <v-text-field
+              <img src="/img/file-upload.jpg" height="150" />
+              <img :src="textUrl" height="150" v-if="textUrl"/>
+              <v-text-field label="เลือกไฟล์  grade.txt" 
+              @click='pickFile' v-model='textName' prepend-icon='attach_file'></v-text-field>
+                <input
+                  type="file"
+                  style="display: none"
+                  ref="image1"
+                  accept="text/*"
+                  @change="onFilePicked"
+                >
+              
+
+
+
+            <!-- <v-text-field
               v-model="user"
               :rules="[() => user.length > 0 || 'This field is required']"
               label="user name"
               required
-            ></v-text-field>
+            ></v-text-field> -->
 
-            <v-text-field
+            <!-- <v-text-field
               v-model="password"
               :rules="[() => password.length > 0 || 'This field is required']"
               label="รหัสผ่าน"
@@ -28,8 +44,8 @@
               label="รหัสผ่านอีกครั้ง "
               persistent-hint
               required
-            ></v-text-field>
-            <small>*ต้องมีค่า ห้ามเว้นว่าง</small>
+            ></v-text-field> -->
+            <!-- <small>*ต้องมีค่า ห้ามเว้นว่าง</small> -->
           </v-card-text>
           <v-card-actions class="flex row justify-center">
             <v-btn color="success" >
@@ -47,11 +63,45 @@
 export default {
   data() {
     return {
+      dialog: false,
+      textName: '',
+      textUrl: '',
+      textFile: '',
       user: '',
       password: '',
       password2: '',
+
     }
   },
+  methods: {
+    pickFile () {
+        this.$refs.image1.click ()
+    },
+		
+		onFilePicked (e) {
+			const files = e.target.files
+			if(files[0] !== undefined) {
+				this.textName = files[0].name
+				if(this.textName.lastIndexOf('.') <= 0) {
+					return
+				}
+				const fr = new FileReader ()
+				fr.readAsDataURL(files[0])
+				fr.addEventListener('load', () => {
+					this.textUrl = fr.result
+					this.textFile = files[0] // this is an text file that can be sent to server...
+				})
+			} else {
+				this.textName = ''
+				this.textFile = ''
+				this.textUrl = ''
+			}
+		},
+
+    
+
+
+  }, //method
  
 
 }
